0b8290d059e6284fe3b072f4d4192734a972349c
dfiliagg <dfiliagg@student.42roma.it>
2022-09-08 11:46:58 +0200

    3try

--- c00/ex01/ft_print_alphabet.c
+++ c00/ex01/ft_print_alphabet.c
@@ -1,7 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfiliagg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 11:11:49 by dfiliagg          #+#    #+#             */
+/*   Updated: 2022/09/08 11:17:05 by dfiliagg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include<unistd.h>
 
-void ft_print_alphabet(){
-
-		write(1,"abcdefghijklmnopqrstuvwxyz", 26);
-
+void	ft_print_alphabet()
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfiliagg <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 11:18:21 by dfiliagg          #+#    #+#             */
+/*   Updated: 2022/09/08 11:22:35 by dfiliagg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include<unistd.h>
 
-void ft_print_reverse_alphabet(){
-
-		write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+void	ft_print_reverse_alphabet()
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
 }
